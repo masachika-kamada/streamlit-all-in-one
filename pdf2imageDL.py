1de999877b35974c614cb327f79eade349435002
masachika <masachika.kmd@gmail.com>
2022-06-21 18:08:40 +0900

    PDF画像変換の機能要求を記載

--- pdf2imageDL.py
+++ pdf2imageDL.py
@@ -2,6 +2,17 @@ import streamlit as st
 import pdf2image
 
 
+"""
+機能要求
+1. PDFをアップロード
+2. プレビュー画像が表示される
+3. ページを選択する（全選択もつける）
+4. 保存ボタンを押す
+5. 1ページのみの時はJPGファイルが保存される
+6. 複数ページの時はZIPファイルが保存される
+"""
+
+
 def run():
     file = st.file_uploader("翻訳したいpdfファイルをアップロードしてください", type=['pdf'])
     file_path = "./src.pdf"
