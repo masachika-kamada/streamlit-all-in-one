2ab5e34c6af71725fea6dffe39cc9f0592c1c6bb
masachika <masachika.kmd@gmail.com>
2022-06-21 17:26:05 +0900

    sidebarを表示

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import streamlit as st
 
 
+def sidebar():
+    with st.sidebar:
+        func_select = st.radio(
+            "機能切替",
+            ("PDFを画像に変換", "画像をリサイズ", "画像サイズの表示")
+        )
+
+    return func_select
+
+
 def main():
     st.markdown('# Image Process all in one')
+    func = sidebar()
+
+    if func == "PDFを画像に変換":
+        pass
+    elif func == "画像をリサイズ":
+        pass
+    elif func == "画像サイズの表示":
+        pass
 
 
 if __name__ == '__main__':
